29e7311423cda6d57df69c3114db07040037bd2d
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-27 12:18:42 -0300

    operadores do PHP

---
+++ operadores.php
@@ -0,0 +1,17 @@
+<?php
+
+$idade = 19;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$potencia = 2 ** 3;
+$restoDaDivisao = 10 %  3;
+
+
+echo $idadeDaqui10Anos;
+
