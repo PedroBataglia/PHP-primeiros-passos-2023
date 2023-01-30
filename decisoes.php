20c4a0d5f5c212aa333dffd2f3fe7dbcac435705
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-30 11:16:52 -0300

    utilizando else if

--- decisoes.php
+++ decisoes.php
@@ -1,13 +1,14 @@
 <?php
 
-$idade = 16;
-$nome = "Pedro";
+$idade = 17;
+$numeroDePessoas = 1;
 
 echo "Você só pode entrar se tiver mais de 18 anos ou";
 echo "se tiver 16 anos e estiver acompanhado.". PHP_EOL;
-if ($idade >= 18 && $nome == "Pedro") {
-    echo "Você tem $idade anos.". PHP_EOL;
-    echo "Pode entrar";
+if ($idade >= 18) {
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} else if($idade >= 16 && $numeroDePessoas > 1) {
+        echo "Você tem $idade anos, está acompanhado(a), então pode entrar";
 } else {
     echo "Você só tem $idade anos.".PHP_EOL;
     echo "você não pode entrar!";
