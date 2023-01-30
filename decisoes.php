646dbfc636131e6d53f374cb709e892b601b7094
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-30 11:05:19 -0300

    condicionais com operadores

--- decisoes.php
+++ decisoes.php
@@ -1,11 +1,14 @@
 <?php
 
-$idade = 19;
+$idade = 16;
 $nome = "Pedro";
 
-echo "Você só pode entrar se tiver mais de 18 anos". PHP_EOL;
-
+echo "Você só pode entrar se tiver mais de 18 anos ou";
+echo "se tiver 16 anos e estiver acompanhado.". PHP_EOL;
 if ($idade >= 18 && $nome == "Pedro") {
     echo "Você tem $idade anos.". PHP_EOL;
     echo "Pode entrar";
+} else {
+    echo "Você só tem $idade anos.".PHP_EOL;
+    echo "você não pode entrar!";
 }
