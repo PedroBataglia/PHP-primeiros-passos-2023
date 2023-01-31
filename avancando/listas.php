ebab8e040395b4f36b150c3357c0c82835a8d165
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-31 08:34:37 -0300

    array em PHP

---
+++ avancando/listas.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$idadeLista  = array(23, 43, 53, 16, 18, 17, 23, 22, 21, 32);
+//             0    1  2   3   4   5   6   7   8    9
+$listaIdade = [23, 43, 53, 16, 18, 17, 23, 22, 21, 32]; // 10 valores no total
+
+$umaIdade = $listaIdade[2];
+
+echo $umaIdade;
