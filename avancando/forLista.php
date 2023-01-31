751fef5fc2813b8aa96c5acc0c9520fe8c4984a2
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-31 09:09:50 -0300

    tornando mais dinâmico

--- avancando/forLista.php
+++ avancando/forLista.php
@@ -2,8 +2,9 @@
 
 
 $idadeAlunos = [19, 18, 15, 14, 16, 15, 17, 16, 18];
+$total = count($idadeAlunos);
 
-for ($i = 0; $i < 9; $i++) {
+for ($i = 0; $i < $total; $i++) {
     $num = $i + 1;
     echo "A idade do $num é $idadeAlunos[$i]".PHP_EOL;
 }
