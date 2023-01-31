c68feeeb5c7ba148497f748ce35386c9dd06e539
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-31 15:24:54 -0300

    adicionando elementos a arrays associativos

--- avancando/forEachLista.php
+++ avancando/forEachLista.php
@@ -2,24 +2,31 @@
 
 
 $carrosNovos = [
-        123094 => [
+        '123094' => [
             'marca' => 'Ferrari',
             'modelo' => 'F389',
             'ano' => 2018,
             'motor' => 'v8',
         ],
-        343242 => [
+        '343242' => [
     'marca' => 'Fiat',
     'modelo' => 'Fiat 147',
     'ano' => 2007,
     'motor' => 'v2'
-],
-        342323 => [
+        ],
+        '342323' => [
             'marca' => 'volkswagen',
             'modelo' => 'Cross',
             'ano' => 2017,
             'motor' => 'v7'
         ],
+];
+
+$carrosNovos['323422'] = [
+    'marca' => "Lamburguini",
+    'modelo' => "elementor",
+    'ano' => 2022,
+    'motor'  => 'v9',
 ];
 
 foreach ($carrosNovos as $num => $carro) {
