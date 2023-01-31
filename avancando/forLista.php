dbb7025a7a1e5673ab2b24466750511ed6b4b796
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-31 09:05:44 -0300

    for bem feito

--- avancando/forLista.php
+++ avancando/forLista.php
@@ -3,6 +3,7 @@
 
 $idadeAlunos = [19, 18, 15, 14, 16, 15, 17, 16, 18];
 
-for ($i = 0; $i < $idadeAlunos; $i++) {
-    echo "A idade do $i é $idadeAlunos[$i]";
+for ($i = 0; $i < 9; $i++) {
+    $num = $i + 1;
+    echo "A idade do $num é $idadeAlunos[$i]".PHP_EOL;
 }
