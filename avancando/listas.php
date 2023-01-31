8d48fa025a75de8d1810ba6bf19d9039fb8079e9
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-31 15:24:19 -0300

    adicionando elementos em uma lista PHP

--- avancando/listas.php
+++ avancando/listas.php
@@ -5,6 +5,12 @@ $idadeLista  = array(23, 43, 53, 16, 18, 17, 23, 22, 21, 32);
 //             0    1  2   3   4   5   6   7   8    9
 $listaIdade = [23, 43, 53, 16, 18, 17, 23, 22, 21, 32]; // 10 valores no total
 
+$listaIdade[] = 20;
+
+
 $umaIdade = $listaIdade[2];
 
 echo $umaIdade;
+foreach($listaIdade as $idade) {
+    echo $idade.PHP_EOL;
+}
