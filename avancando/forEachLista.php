aea9f853eb2fc46223a2606661a5deaa4b459b11
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-31 12:29:57 -0300

    melhorando a logica de programacao

--- avancando/forEachLista.php
+++ avancando/forEachLista.php
@@ -1,30 +1,31 @@
 <?php
 
-$carro1 = [
-    'marca' => 'Ferrari',
-    'modelo' => 'F389',
-    'ano' => 2018,
-    'motor' => 'v8',
-];
 
-$carro2 = [
+$carrosNovos = [
+        123094 => [
+            'marca' => 'Ferrari',
+            'modelo' => 'F389',
+            'ano' => 2018,
+            'motor' => 'v8',
+        ],
+        343242 => [
     'marca' => 'Fiat',
     'modelo' => 'Fiat 147',
     'ano' => 2007,
     'motor' => 'v2'
+],
+        342323 => [
+            'marca' => 'volkswagen',
+            'modelo' => 'Cross',
+            'ano' => 2017,
+            'motor' => 'v7'
+        ],
 ];
 
-$carro3 = [
-    'marca' => 'volkswagen',
-    'modelo' => 'Cross',
-    'ano' => 2017,
-    'motor' => 'v7'
-];
-
-$carrosNovos = [$carro1, $carro2, $carro3];
-
-foreach ($carrosNovos as $carro) {
+foreach ($carrosNovos as $num => $carro) {
+    echo " Modelo de número $num:".PHP_EOL;
     echo $carro['modelo'].PHP_EOL;
     echo $carro['ano'].PHP_EOL;
     echo $carro['motor'].PHP_EOL;
+
 }
