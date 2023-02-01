53e5f070fc370f60f5bde08481484379fc2a1080
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-01 12:01:09 -0300

    adicionando funcionalidades

---
+++ avancando/functionString.php
@@ -0,0 +1,22 @@
+<?php
+
+function escreverNome($nome) {
+    echo "Meu nome é $nome".PHP_EOL;
+}
+
+escreverNome("Pedro");
+
+$pessoa = [
+  'nome' => 'Pedro',
+  'idade' => 19,
+  'relacionamento' => 'solteiro',
+  'profissao' => 'programador',
+];
+
+function exibeArray($lista) {
+    foreach ($lista as $item) {
+        echo "$item".PHP_EOL;
+    }
+}
+
+exibeArray($pessoa);
