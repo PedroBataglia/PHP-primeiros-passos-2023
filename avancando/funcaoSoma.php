38fb2fc0b1025b2e1ff07d32f3be3f1573a6ddee
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-01 09:55:40 -0300

    funcao de tabuada

--- avancando/funcaoSoma.php
+++ avancando/funcaoSoma.php
@@ -6,4 +6,13 @@ function soma($numA, $numB) {
 
 $somandoNumeros = soma(2, 2);
 
-echo $somandoNumeros;
+echo $somandoNumeros.PHP_EOL;
+
+function tabuada($num) {
+    for ($i = 1; $i < 11; $i++) {
+        $total = $i * $num;
+        echo "$i x $num = $total".PHP_EOL;
+    }
+}
+
+tabuada($somandoNumeros);
