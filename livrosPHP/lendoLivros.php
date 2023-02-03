c7065e463e927a60542bac4cecbdcafb3531a1d8
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-03 09:03:19 -0300

    lendo objetos com estruturas de repeticao

--- livrosPHP/lendoLivros.php
+++ livrosPHP/lendoLivros.php
@@ -1,3 +1,29 @@
 <?php
 
+$listaDeLivros = [
+    'livro1' => [
+        'Nome' => 'O nome do vento',
+        'Autor' => 'Patrick Rothfuss',
+        'Ano' =>  2010,
+        'Nota' => 10,
+    ],
+    'livro2' => [
+        'Nome' => 'Percy Jackson',
+        'Autor' => 'Rick Riordan',
+        'Ano' => 2005,
+        'Nota' => 6,
+    ],
+    'livro3' => [
+        'Nome' => 'Hobbit',
+        'Autor' => 'J.R.R. Tolkien',
+        'Ano' => 1938,
+        'Nota' => 9,
+    ]
+];
+
+foreach ($listaDeLivros as $num => $livro) {
+    echo "$num:".PHP_EOL;
+    echo $livro['Nome'].PHP_EOL;
+    echo $livro['Autor'].PHP_EOL;
+}
 
