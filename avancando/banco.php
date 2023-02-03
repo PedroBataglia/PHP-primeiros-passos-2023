1e6b6f2d5de0ba32b679cbb081476381b2be3ea9
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-03 16:47:08 -0300

    deselegante porém funcional

--- avancando/banco.php
+++ avancando/banco.php
@@ -36,3 +36,25 @@ foreach ($contasCorrentes as $cpf => $conta) {
     exibeConta($conta);
 }
 echo "</ul>";
+?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contas Correntes</title>
+</head>
+<body>
+<h1>Contas Correntes</h1>
+
+<dl>
+    <?php foreach ($contasCorrentes as $cpf => $contas) { ?>
+        <dt><h3><?php echo $conta['titular']?> - <?php echo $cpf;?></h3></dt>
+        <dd> Saldo: <?php echo $conta['saldo'];?></dd>
+    <?php } ?>
+
+</dl>
+</body>
+</html>
