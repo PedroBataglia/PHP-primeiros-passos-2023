2376a2ff245c1530c6b5d84e118a855101950dfc
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-06 16:38:03 -0300

    ordenando arrays

---
+++ arrays/ordem.php
@@ -0,0 +1,16 @@
+<?php
+
+$notas = [
+    10,
+    8,
+    9,
+    7,
+];
+
+$notasOrdenadas = $notas;
+sort($notasOrdenadas);
+echo "desordenadas".PHP_EOL;
+var_dump($notas);
+
+echo "ordenadas".PHP_EOL;
+var_dump($notasOrdenadas);
