faec258d762023b0270752ab02db1721d24c5e8f
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-07 08:23:53 -0300

    ordenando arrays por parametro do usuario

---
+++ arrays/ordemArrayDeArray.php
@@ -0,0 +1,19 @@
+<?php
+
+$notas = [
+    [
+        'aluno' => 'Maria',
+        'nota' => 10,
+    ],
+    [
+        'aluno' => 'Pedro',
+        'nota' => 9,
+    ],
+    [
+        'aluno' => 'Ana',
+        'nota' => 7,
+    ],
+];
+
+sort($notas);
+var_dump($notas);
