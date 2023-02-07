56ddd3c35da9dad832ea5d369c64efd0a86f8cf0
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-07 13:07:05 -0300

    ordenando lista por parametro especifico

--- arrays/ordemArrayDeArray.php
+++ arrays/ordemArrayDeArray.php
@@ -15,5 +15,18 @@ $notas = [
     ],
 ];
 
-sort($notas);
+function ordenaNotas(array $nota1, array $nota2): int
+{
+    if ($nota1['nota'] > $nota2['nota']) {
+        return -1;
+    }
+    if ($nota1['nota'] < $nota2['nota']) {
+        return 1;
+    }
+    return 0;
+};
+
+usort($notas, 'ordenaNotas');
 var_dump($notas);
+
+
