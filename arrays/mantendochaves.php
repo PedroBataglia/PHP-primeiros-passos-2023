789a652b12c8ae24540a1e737996e6c7342050b5
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-09 10:13:22 -0300

    verificando arrays em lista

--- arrays/mantendochaves.php
+++ arrays/mantendochaves.php
@@ -13,7 +13,16 @@ ksort($notas);
 var_dump($notas);
 
 if (gettype($notas) === 'array') {
-    echo "Sim, isso é um array";
+    echo "Sim, isso é um array".PHP_EOL;
 } else {
-    echo "Isso não é um array";
+    echo "Isso não é um array".PHP_EOL;
 }
+
+
+function funcao(array $valor) {
+    for ($i = 0; $i < count($valor); $i++) {
+        echo $valor[$i];
+    }
+}
+
+var_dump(array_is_list($notas));
