3485ab85a90bef67a161540d669eda8082458dbb
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-02-14 14:53:38 -0300

    verficando a diferenca entre chave e valor

--- arrays/verificandoValores.php
+++ arrays/verificandoValores.php
@@ -1,1 +1,18 @@
 <?php
+
+$notas = [
+    'Ana' => '10',
+    'João' => '8',
+    'Pedro' => '9',
+    'Miguel' => '6',
+    'Felipe' => '7',
+];
+
+echo "Alguém tirou 10?".PHP_EOL;
+var_dump(in_array(10, $notas));
+
+// array_key_exists = verifica se a chave existe
+
+// in_array = verifica se o valor está presente naquele array
+
+// inset = verifica se a chave existe e o valor não é nullo
