f8e5e0114d008b684fe0165000551a9c627d1291
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-03-06 11:56:36 -0300

    sim

--- Strings/psoicao.php
+++ Strings/psoicao.php
@@ -1,1 +1,8 @@
 <?php
+
+$email = "Pedro@hotmail.com.br";
+
+$posicaoDoArroba = strpos($email, '@');
+
+echo substr($email, 0, $posicaoDoArroba).PHP_EOL;
+echo substr($email, $posicaoDoArroba+1).PHP_EOL;
