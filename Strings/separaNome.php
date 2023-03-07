5d50683c09794216562defaf48bf2853a6c9f14d
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-03-07 11:54:38 -0300

    separando por espacos

--- Strings/separaNome.php
+++ Strings/separaNome.php
@@ -1,8 +1,13 @@
 <?php
 
-$nomeCompleto = 'Pedro Petretti';
+$nomeCompleto = 'Pedro Henrique Petretti Bataglia';
 
 $espaco = strpos($nomeCompleto, ' ');
 
-echo substr($nomeCompleto, 0, $espaco).PHP_EOL;
-echo substr($nomeCompleto, $espaco + 1);
+//echo substr($nomeCompleto, 0, $espaco).PHP_EOL;
+//echo substr($nomeCompleto, $espaco + 1);
+
+var_dump(explode(' ', $nomeCompleto));
+$nomeItaliano = explode(' ', $nomeCompleto);
+
+echo "$nomeItaliano[0] $nomeItaliano[3]";
