ab3c065b134659af81be442154465c9fbfd5610e
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-03-08 11:55:46 -0300

    funcoes manipulandostrings

--- Strings/novoFuncionario.php
+++ Strings/novoFuncionario.php
@@ -1,1 +1,34 @@
 <?php
+
+function boasVindas(string $name, string $profissao, float $salario)
+{
+    $mensagem = <<<FNIAL
+Olá, $name, seja bem-vindo!
+
+Você foi contratado pela Hyle Tecnology and colsulting
+para a vaga de $profissao, com o salário de $salario.
+
+Você começa no dia 02/06,
+    {assinado}
+FNIAL;
+
+    echo $mensagem;
+
+}
+
+function empresaNova(string $empresaNome, string $empresaLocal, string $empresaDiretor)
+{
+    $mensagemEmpresa = <<<FINAL
+Olá $empresaDiretor, dono da empresa $empresaNome.
+
+Gostariamos de propor a construção de uma nova filial no $empresaLocal
+Nos retorne o mais rápido possível.
+
+FINAL;
+
+    echo $mensagemEmpresa;
+}
+
+empresaNova('Hyle', 'Rua Krypiton 1390', 'Kleber');
+
+
