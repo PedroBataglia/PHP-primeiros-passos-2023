d42c56bb47393c13cfad4a884c4f551aeab159c7
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-16 10:09:22 -0300

    agora podemos realizar funcoes em todos os elementos do array

--- arrays/ArrayEspanhol.php
+++ arrays/ArrayEspanhol.php
@@ -2,5 +2,18 @@
 
 function mostrarEspanhol(int $numero, string $word)
 {
-    
+    return "O número {$numero} é chamado de {$word} em Espanhol";
 }
+function mapEspanhol(int $numero, string $word)
+{
+    return [$numero => $word];
+}
+
+
+$numeros = [1, 2, 3, 4, 5];
+$palavras = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+
+$resultado = array_map('mostrarEspanhol', $numeros, $palavras);
+print_r($resultado);
+$geral = array_map('mapEspanhol', $numeros, $palavras);
+print_r($geral);
