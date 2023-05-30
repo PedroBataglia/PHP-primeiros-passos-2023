842a06711090ebbf27a7b6776a5ee8e48cabd473
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-29 21:44:09 -0300

    manipulando chaves de arrays

--- arrays/arrayKeyExists.php
+++ arrays/arrayKeyExists.php
@@ -1,1 +1,13 @@
 <?php
+
+$funcionarios = [
+    'Diretor' => 'Carlos',
+    'Gerente' => 'André',
+    'Programador' => 'Samira',
+];
+
+if (array_key_exists('Programador', $funcionarios)) {
+    echo "Chave existe";
+} else {
+    echo "Chave não existe";
+}
---
+++ arrays/fixedArray.php
@@ -0,0 +1,10 @@
+<?php
+// Initialize the array with a fixed length
+$array = new SplFixedArray(5);
+
+$array[1] = 2;
+$array[4] = "foo";
+
+var_dump($array[0]); // NULL
+var_dump($array[1]); // int(2)
+var_dump($array);
